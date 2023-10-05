3f175660c56c5fa9f87d1911fbff3c1a20b479b2
Andrey Shalashov <avshalashov@yandex.ru>
2023-10-05 19:29:17 +0300

    read migrations from embed fs

--- internal/adapter/database/database.go
+++ internal/adapter/database/database.go
@@ -3,16 +3,19 @@ package database
 import (
 	"context"
 	"database/sql"
+	"embed"
 	"errors"
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5/pgconn"
 	_ "github.com/jackc/pgx/v5/stdlib"
 
+	"github.com/k1nky/gophermart/internal/entity/order"
 	"github.com/k1nky/gophermart/internal/entity/user"
 )
 
@@ -23,6 +26,9 @@ const (
 var (
 	ErrUniqueViolation = errors.New("duplicate key value")
 )
+
+//go:embed migrations/*.sql
+var migrationsFS embed.FS
 
 type Adapter struct {
 	*sql.DB
@@ -42,7 +48,11 @@ func (a *Adapter) Open(dsn string) (err error) {
 }
 
 func (a *Adapter) Initialize(dsn string) error {
-	m, err := migrate.New("file://db/migrations", dsn)
+	source, err := iofs.New(migrationsFS, "migrations")
+	if err != nil {
+		return err
+	}
+	m, err := migrate.NewWithSourceInstance("iofs", source, dsn)
 	if err != nil {
 		return err
 	}
@@ -57,7 +67,9 @@ func (a *Adapter) GetUser(ctx context.Context, login string) (*user.User, error)
 	u := &user.User{
 		Login: login,
 	}
-	row := a.QueryRowContext(ctx, `SELECT password FROM users WHERE login=$1`, login)
+
+	const query = `SELECT password FROM users WHERE login=$1`
+	row := a.QueryRowContext(ctx, query, login)
 	if err := row.Err(); err != nil {
 		return nil, err
 	}
@@ -72,17 +84,16 @@ func (a *Adapter) GetUser(ctx context.Context, login string) (*user.User, error)
 
 func (a *Adapter) NewUser(ctx context.Context, u user.User) (*user.User, error) {
 
-	row := a.QueryRowContext(ctx, `
+	const query = `
 		INSERT INTO users AS u (login, password)
 		VALUES ($1, $2)
 		RETURNING u.user_id
-	`, u.Login, u.Password)
+	`
+
+	row := a.QueryRowContext(ctx, query, u.Login, u.Password)
 	if err := row.Err(); err != nil {
-		var pgerr *pgconn.PgError
-		if errors.As(err, &pgerr) {
-			if pgerrcode.IsIntegrityConstraintViolation(pgerr.Code) {
-				return nil, fmt.Errorf("%s %w", u.Login, ErrUniqueViolation)
-			}
+		if a.hasUniqueViolationError(err) {
+			return nil, fmt.Errorf("%s %w", u.Login, ErrUniqueViolation)
 		}
 		return nil, err
 	}
@@ -90,6 +101,37 @@ func (a *Adapter) NewUser(ctx context.Context, u user.User) (*user.User, error) 
 		return nil, err
 	}
 	return &u, nil
+}
+
+func (a *Adapter) NewOrder(ctx context.Context, u user.User, o order.Order) (*order.Order, error) {
+	const query = `
+		INSERT INTO orders AS o (user_id, number, status, uploaded_at)
+		VALUES ($1, $2, 'NEW', NOW())
+		RETURNING o.order_id, o.uploaded_at
+	`
+	row := a.QueryRowContext(ctx, query, u.ID, o.Number)
+	if err := row.Err(); err != nil {
+		if a.hasUniqueViolationError(err) {
+			return nil, fmt.Errorf("%s %w", o.Number, ErrUniqueViolation)
+		}
+		return nil, err
+	}
+	if err := row.Scan(&o.ID, &o.UploadedAt); err != nil {
+		return nil, err
+	}
+	o.Status = order.StatusNew
+	return &o, nil
+
+}
+
+func (a *Adapter) hasUniqueViolationError(err error) bool {
+	var pgerr *pgconn.PgError
+	if errors.As(err, &pgerr) {
+		if pgerrcode.IsIntegrityConstraintViolation(pgerr.Code) {
+			return true
+		}
+	}
+	return false
 }
 
 func (a *Adapter) IsUniqueViolation(err error) bool {
